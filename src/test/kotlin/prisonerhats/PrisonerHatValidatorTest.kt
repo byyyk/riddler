af8a3c483133911dc47e10b8b9e81d9491977f67
Mateusz Kulpa <m.c.kulpa@gmail.com>
2022-03-18 20:24:15 +0100

    test fixes

--- src/test/kotlin/prisonerhats/PrisonerHatValidatorTest.kt
+++ src/test/kotlin/prisonerhats/PrisonerHatValidatorTest.kt
@@ -9,23 +9,23 @@ internal class PrisonerHatValidatorTest {
         val validator = PrisonerHatValidator()
         assertEquals(true, validator.validate(
             WornHats(listOf(HatColor.BLACK)),
-            SpokenHats(listOf(HatColor.BLACK))
+            SpokenHats(mutableListOf(HatColor.BLACK))
         ))
         assertEquals(false, validator.validate(
             WornHats(listOf(HatColor.BLACK, HatColor.WHITE)),
-            SpokenHats(listOf(HatColor.BLACK, HatColor.WHITE))
+            SpokenHats(mutableListOf(HatColor.BLACK, HatColor.WHITE))
         ))
         assertEquals(true, validator.validate(
             WornHats(listOf(HatColor.BLACK, HatColor.WHITE)),
-            SpokenHats(listOf(HatColor.WHITE, HatColor.BLACK))
+            SpokenHats(mutableListOf(HatColor.WHITE, HatColor.BLACK))
         ))
         assertEquals(false, validator.validate(
             WornHats(listOf(HatColor.BLACK)),
-            SpokenHats(listOf(HatColor.WHITE))
+            SpokenHats(mutableListOf(HatColor.WHITE))
         ))
         assertEquals(false, validator.validate(
             WornHats(listOf(HatColor.BLACK)),
-            SpokenHats(listOf(HatColor.BLACK, HatColor.BLACK))
+            SpokenHats(mutableListOf(HatColor.BLACK, HatColor.BLACK))
         ))
     }
 }
